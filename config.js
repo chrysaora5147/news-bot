c3a56db98c4b06160c152c30fe51ea7547b678e3
Nuttachai Pornkittiya <boss@MacBook-Air-khxng-Nuttachai.local>
2026-06-29 19:02:54 +0700

    Connect frontend to Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.NEWS_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://zaqvrwsooxdtkepaaunk.supabase.co",
+  supabaseAnonKey: "sb_publishable_3dgHmUZAaQyq8SPkPT00-Q_48oE7b3K",
 };
